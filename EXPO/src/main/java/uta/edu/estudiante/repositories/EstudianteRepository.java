61ce8589fd824bbe025d2fb943da6a19acde8098
MarcoLopez2020 <74079451+MarcoLopez2020@users.noreply.github.com>
2024-01-01 23:15:30 -0500

    Delete EXPO directory

--- EXPO/src/main/java/uta/edu/estudiante/repositories/EstudianteRepository.java
+++
@@ -1,9 +0,0 @@
-package uta.edu.estudiante.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import uta.edu.estudiante.models.Estudiante;
-
-@Repository
-public interface EstudianteRepository extends JpaRepository<Estudiante, String> {
-}
